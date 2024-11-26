609dc3d95b64d9c76e66c5b54592ad776a2c7f1c
Rick <rmaas939@gmail.com>
2024-11-26 20:54:56 +0100

    Create News Item

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,6 +14,10 @@ $routes->get('/', 'Home::index');
 $routes->get('news', [News::class, 'index']);
 $routes->get('news/(:segment)', [News::class, 'show']);
 
+//add news
+$routes->get('news/new', [News::class, 'new']);
+$routes->post('news', [News::class, 'create']);
+
 //pages
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
